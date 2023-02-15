f76f51459342f5b283403d64cc764595c2b8ace1
MrUnwonted <arjungodzarjun@gmail.com>
2023-02-15 10:15:56 +0530

    jquey working fine

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
@@ -86,4 +86,11 @@ public class UserRepositoryTests {
 		 Integer userId = 2;
 		 repo.deleteById(userId);
 	 }
+
+	 @Test
+	public void testGetUserByEmail(){
+		String email = "ravi@gmail.com";
+		User user =repo.getUserByEmail(email);
+		assertThat(user).isNotNull();
+	 }
 }
