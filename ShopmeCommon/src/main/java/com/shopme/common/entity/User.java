2b103f746a8cb0d7d220fd88871eccd43cb25850
MrUnwonted <arjungodzarjun@gmail.com>
2023-02-16 19:15:36 +0530

    after authentication

--- ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
@@ -30,7 +30,7 @@ public class User {
 
 	private boolean enabled;
 
-	@ManyToMany()
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
 
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -2,9 +2,11 @@ package com.shopme.admin.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,23 +15,43 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		return new ShopmeUserDetailsService();
+	}
+	
+	
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    protected PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
+    public DaoAuthenticationProvider authenticationProvider() {
+    	DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    	authProvider.setUserDetailsService(userDetailsService());
+    	authProvider.setPasswordEncoder(passwordEncoder());
+    	
+    	return authProvider;
+    }
+    
+    
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+    	auth.authenticationProvider(authenticationProvider());
+    }
+    
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .anyRequest().permitAll();
-//                .authenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .usernameParameter("email")
-//                .permitAll()
-//                .and()
-//                .csrf().ignoringRequestMatchers("/images/**", "/js/**", "/webjars/**");
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+//                .defaultSuccessUrl("/")
+                .usernameParameter("email")
+                .permitAll()
+                .and()
+                .csrf().ignoringRequestMatchers("/images/**", "/js/**", "/webjars/**");
         return http.build();
     }
     
